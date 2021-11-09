763c854a8a099f850d8915ca35699e6f690a5e85
Pukil <53221134+Pukil@users.noreply.github.com>
2021-11-09 17:26:08 +0100

    Update settings.py

--- kcal/settings.py
+++ kcal/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tpx&_srg^v3!b7%!hn7%3_wo!f&zyrp_&=zjt(0+1d9+mb+z1i'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
